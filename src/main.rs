5e915fa39344712e732ffd11360a0582eaad1492
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-10-31 22:15:43 +0100

    feat: read excel file and debug print content.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use calamine::{open_workbook, Error, Reader, Xlsx};
+
+fn read_excel(path: &str, sheet: &str) -> Result<(), Error> {
+    let mut workbook: Xlsx<_> = open_workbook(path)?;
+    let range = workbook
+        .worksheet_range(sheet)
+        .ok_or(Error::Msg("Cannot find sheet."))??;
+
+    for row in range.rows() {
+        println!("row={:?}, row[0]={:?}", row, row[0]);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::path::Path::new(file!())
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("tests/data/test_scedule.xlsx");
+    read_excel(path.to_str().unwrap(), "Sheet1").unwrap();
 }
