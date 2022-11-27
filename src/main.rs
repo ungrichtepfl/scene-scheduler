5aec8b417b05653dced95d08b5bcf261ca37642f
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-11-27 10:57:55 +0100

    feat: Parsing time of time scedule.

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,48 @@ impl fmt::Display for ExcelParseError {
 
 impl Error for ExcelParseError {}
 
+#[derive(Debug)]
 pub struct ScheduleEntry {
     pub date: NaiveDate,
-    pub time: NaiveTime,
+    pub start_stop_time: (NaiveTime, Option<NaiveTime>),
     pub scenes: Vec<String>,
 }
 
+impl ScheduleEntry {
+    pub fn new(date: &String, time: &String, scenes: &String) -> ScheduleEntry {
+        dbg!(date, time, scenes);
+        ScheduleEntry {
+            date: ScheduleEntry::parse_date(date),
+            start_stop_time: ScheduleEntry::parse_time(time),
+            scenes: ScheduleEntry::parse_scenes(scenes),
+        }
+    }
+    fn parse_date(date: &String) -> NaiveDate {
+        //TODO:
+        chrono::offset::Local::now().date_naive()
+    }
+    fn parse_time(time: &String) -> (NaiveTime, Option<NaiveTime>) {
+        if time.contains("-") {
+            if let [start, stop] = time.split('-').collect::<Vec<&str>>().as_slice() {
+                (
+                    NaiveTime::parse_from_str(start.trim(), "%H:%M").unwrap(), // TODO: Add error handling
+                    Some(NaiveTime::parse_from_str(stop.trim(), "%H:%M").unwrap()), // TODO: Add error handling
+                )
+            } else {
+                panic!() // TODO: Add error handling
+            }
+        } else {
+            (
+                NaiveTime::parse_from_str(time.trim(), "%H:%M").unwrap(), // TODO: Add error handling
+                None,
+            )
+        }
+    }
+    fn parse_scenes(scenes: &String) -> Vec<String> {
+        // TODO:
+        vec![]
+    }
+}
 pub fn read_excel(path: &str, sheet_num: usize) -> Result<Range<DataType>, calamine::Error> {
     let mut workbook: Xlsx<_> = open_workbook(path)?;
     let range = workbook
@@ -37,19 +73,54 @@ pub fn read_excel(path: &str, sheet_num: usize) -> Result<Range<DataType>, calam
 }
 
 pub fn parse_schedule_plan_content(
-    excel_range: Range<DataType>,
+    excel_range: &Range<DataType>,
 ) -> Result<Vec<ScheduleEntry>, ExcelParseError> {
+    let mut start_parsing = false;
+    let mut previous_date: Option<String> = None;
+    let mut scedule_entries = vec![];
     for row in excel_range.rows() {
         if row.len() != 3 {
-            todo!("Add parser error"); // TODO:
+            todo!("Add parser error when more than 3 rows."); // TODO:
         }
-        // TODO:
+        let (opt_date, times, scenes) = match row {
+            [DataType::String(x), DataType::String(y), DataType::String(z)] => {
+                (Some(x.to_owned()), y.to_owned(), z.to_owned())
+            }
+            [DataType::String(x), DataType::String(y), DataType::Float(z)] => {
+                (Some(x.to_owned()), y.to_owned(), z.to_string())
+            }
+            [DataType::Empty, DataType::String(y), DataType::Float(z)] => {
+                (None, y.to_owned(), z.to_string())
+            }
+            [DataType::Empty, DataType::String(y), DataType::String(z)] => {
+                (None, y.to_owned(), z.to_owned())
+            }
+            _ => {
+                todo!("Add parser error when not only strings. Row: {row:?}")
+            }
+        };
+
+        if !start_parsing {
+            if !times.eq(&String::from("Zeit")) {
+                continue;
+            } else {
+                start_parsing = true;
+                continue;
+            }
+        }
+        if let Some(date) = &opt_date {
+            previous_date = Some(date.clone());
+            scedule_entries.push(ScheduleEntry::new(&date, &times, &scenes));
+        } else if let Some(date) = &previous_date {
+            scedule_entries.push(ScheduleEntry::new(&date, &times, &scenes));
+        } else {
+            panic!("No date found")
+        }
     }
-    // TODO:
-    Ok(vec![])
+    Ok(scedule_entries)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let root_dir = Path::new(file!()).parent().and_then(|p| p.parent()).expect(
         format!(
             "Root file path not found. File '{}' has probably moved.",
@@ -59,13 +130,14 @@ fn main() {
     );
     let test_file_path = root_dir.join("tests/data/test_scedule.xlsx");
     let sheet_num = 0;
-    if let Err(e) = read_excel(
+    let excel_range = read_excel(
         test_file_path
             .to_str()
             .expect("Check file name, wrong UTF-8 encoding for this os."),
         sheet_num,
-    ) {
-        println!("Error while reading excel: {e}");
-        std::process::exit(1);
-    }
+    )?;
+    let scedule_entries = parse_schedule_plan_content(&excel_range)?;
+    dbg!(scedule_entries);
+
+    Ok(())
 }
