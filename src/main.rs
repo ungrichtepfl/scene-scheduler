74e83ca4b5614104dc1834b69c78dcc3522f6be5
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-11-27 12:47:13 +0100

    feat: Scene parser added. Improved time parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables)] // TODO: Remove when stable
+
 use calamine::{open_workbook, DataType, Range, Reader, Xlsx};
 use chrono::format::ParseError;
 use chrono::{NaiveDate, NaiveTime};
@@ -24,46 +26,81 @@ impl Error for ExcelParseError {}
 
 #[derive(Debug)]
 pub struct ScheduleEntry {
-    pub date: NaiveDate,
+    pub date: Option<NaiveDate>,
     pub start_stop_time: (NaiveTime, Option<NaiveTime>),
     pub scenes: Vec<String>,
 }
 
-impl ScheduleEntry {
-    pub fn new(date: &String, time: &String, scenes: &String) -> ScheduleEntry {
-        dbg!(date, time, scenes);
-        ScheduleEntry {
-            date: ScheduleEntry::parse_date(date),
-            start_stop_time: ScheduleEntry::parse_time(time),
-            scenes: ScheduleEntry::parse_scenes(scenes),
-        }
-    }
-    fn parse_date(date: &String) -> NaiveDate {
-        //TODO:
-        chrono::offset::Local::now().date_naive()
-    }
-    fn parse_time(time: &String) -> (NaiveTime, Option<NaiveTime>) {
-        if time.contains("-") {
-            if let [start, stop] = time.split('-').collect::<Vec<&str>>().as_slice() {
-                (
-                    NaiveTime::parse_from_str(start.trim(), "%H:%M").unwrap(), // TODO: Add error handling
-                    Some(NaiveTime::parse_from_str(stop.trim(), "%H:%M").unwrap()), // TODO: Add error handling
-                )
-            } else {
-                panic!() // TODO: Add error handling
-            }
-        } else {
-            (
-                NaiveTime::parse_from_str(time.trim(), "%H:%M").unwrap(), // TODO: Add error handling
-                None,
-            )
-        }
-    }
-    fn parse_scenes(scenes: &String) -> Vec<String> {
-        // TODO:
-        vec![]
+pub fn get_scedule_entry(date: &String, time: &String, scenes: &String) -> ScheduleEntry {
+    dbg!(date, time, scenes);
+    ScheduleEntry {
+        date: parse_date(date),
+        start_stop_time: parse_time(time),
+        scenes: parse_scenes(scenes),
     }
 }
+fn parse_date(date: &String) -> Option<NaiveDate> {
+    //TODO:
+
+    Some(chrono::offset::Local::now().date_naive())
+}
+
+fn parse_time(time: &String) -> (NaiveTime, Option<NaiveTime>) {
+    if time.contains("-") {
+        if let [start, stop] = time.split('-').collect::<Vec<&str>>().as_slice() {
+            (
+                NaiveTime::parse_from_str(start.trim(), "%H:%M").unwrap(), // TODO: Add error handling
+                Some(NaiveTime::parse_from_str(stop.trim(), "%H:%M").unwrap()), // TODO: Add error handling
+            )
+        } else {
+            panic!() // TODO: Add error handling
+        }
+    } else {
+        (
+            NaiveTime::parse_from_str(time.trim(), "%H:%M").unwrap(), // TODO: Add error handling
+            None,
+        )
+    }
+}
+
+fn add_corresponding_stop_time(scedule_entries: Vec<ScheduleEntry>) -> Vec<ScheduleEntry> {
+    if scedule_entries.len() <= 1 {
+        return scedule_entries;
+    }
+
+    dbg!(scedule_entries.len());
+    let mut previous_start_time = scedule_entries.last().unwrap().start_stop_time.0.clone();
+    let mut previous_date = scedule_entries.last().unwrap().date.clone();
+    let mut new_scedule_entries = vec![];
+
+    for (i, entry) in scedule_entries.into_iter().rev().enumerate() {
+        let previous_date_tmp = entry.date.clone();
+        let previous_start_time_tmp = entry.start_stop_time.0.clone();
+        if !previous_date.eq(&entry.date) || i == 0 {
+            new_scedule_entries.push(entry);
+        } else {
+            if entry.start_stop_time.1.is_none() {
+                new_scedule_entries.push(ScheduleEntry {
+                    start_stop_time: (entry.start_stop_time.0, Some(previous_start_time)),
+                    ..entry
+                })
+            } else {
+                new_scedule_entries.push(entry);
+            }
+        }
+        previous_date = previous_date_tmp;
+        previous_start_time = previous_start_time_tmp;
+    }
+    dbg!(new_scedule_entries.len());
+
+    new_scedule_entries.reverse();
+    new_scedule_entries
+}
+
+fn parse_scenes(scenes: &String) -> Vec<String> {
+    scenes.split("/").map(|s| s.trim().to_owned()).collect()
+}
+
 pub fn read_excel(path: &str, sheet_num: usize) -> Result<Range<DataType>, calamine::Error> {
     let mut workbook: Xlsx<_> = open_workbook(path)?;
     let range = workbook
@@ -110,14 +147,14 @@ pub fn parse_schedule_plan_content(
         }
         if let Some(date) = &opt_date {
             previous_date = Some(date.clone());
-            scedule_entries.push(ScheduleEntry::new(&date, &times, &scenes));
+            scedule_entries.push(get_scedule_entry(&date, &times, &scenes));
         } else if let Some(date) = &previous_date {
-            scedule_entries.push(ScheduleEntry::new(&date, &times, &scenes));
+            scedule_entries.push(get_scedule_entry(&date, &times, &scenes));
         } else {
             panic!("No date found")
         }
     }
-    Ok(scedule_entries)
+    Ok(add_corresponding_stop_time(scedule_entries))
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
