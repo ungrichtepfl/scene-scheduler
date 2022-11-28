3c2609cb0482dd9feab5065165ec184f9ba5e4ea
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-11-28 16:20:46 +0100

    feat: Write ics files.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#![allow(unused_variables)] // TODO: Remove when stable
-
 use calamine::{open_workbook, DataType, Range, Reader, Xlsx};
-use chrono::format::ParseError;
-use chrono::{NaiveDate, NaiveTime};
+// use chrono::format::ParseError;
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeZone};
+use ics::properties::{Description, DtEnd, DtStart, Status, Summary};
+use ics::{escape_text, Event, ICalendar};
+use std::collections::HashSet;
 use std::error::Error;
 use std::fmt;
 use std::path::Path;
+
+type Person = String;
 
 #[derive(Debug)]
 pub struct ExcelParseError {
@@ -27,7 +30,7 @@ impl Error for ExcelParseError {}
 #[derive(Debug)]
 pub struct SceneEntry {
     pub role: String,
-    pub who: String,
+    pub who: Person,
     pub scenes: Vec<String>,
     pub silent_play: Vec<bool>,
 }
@@ -37,6 +40,7 @@ pub struct ScheduleEntry {
     pub date: Option<NaiveDate>,
     pub start_stop_time: (NaiveTime, Option<NaiveTime>),
     pub scenes: Vec<String>,
+    pub uuid: md5::Digest,
 }
 
 pub fn get_schedule_entry(date: &String, time: &String, scenes: &String) -> ScheduleEntry {
@@ -45,8 +49,10 @@ pub fn get_schedule_entry(date: &String, time: &String, scenes: &String) -> Sche
         date: parse_date(date),
         start_stop_time: parse_time(time),
         scenes: parse_scenes(scenes),
+        uuid: md5::compute(date.to_owned() + time.as_str() + scenes.as_str()),
     }
 }
+
 fn parse_date(date: &String) -> Option<NaiveDate> {
     if !["Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa.", "So."]
         .iter()
@@ -257,6 +263,103 @@ fn parse_scene_plan_content(
     }
     Ok(scene_entries)
 }
+fn get_person_to_scene_and_schedule_entry<'a>(
+    schedule_to_scene_entries: &'a Vec<(&'a ScheduleEntry, &'a SceneEntry)>,
+) -> Vec<(Person, Vec<&'a (&'a ScheduleEntry, &'a SceneEntry)>)> {
+    let all_persons = schedule_to_scene_entries
+        .iter()
+        .map(|(_, scene_entry)| scene_entry.who.to_owned())
+        .collect::<HashSet<Person>>();
+    dbg!(&all_persons);
+    let mut person_to_scene_and_schedule_entry = vec![];
+    for person in all_persons {
+        let schedule_entries_for_person = schedule_to_scene_entries
+            .into_iter()
+            .filter(|(_, scene_entry)| scene_entry.who == person)
+            .collect();
+        person_to_scene_and_schedule_entry.push((person, schedule_entries_for_person));
+    }
+    person_to_scene_and_schedule_entry
+}
+
+use chrono::Duration;
+use chrono_tz::Europe::Zurich;
+
+fn get_start_and_end_time(schedule_entry: &ScheduleEntry) -> Option<(String, String)> {
+    if let Some(start_date_naive) = &schedule_entry.date {
+        let (start_time, stop_time_opt) = &schedule_entry.start_stop_time;
+        let start_date_time_naive =
+            NaiveDateTime::new(start_date_naive.to_owned(), start_time.to_owned());
+
+        let start_date = Zurich.from_local_datetime(&start_date_time_naive).unwrap(); // TODO: Better error handling start time
+        let stop_date = if let Some(stop_time) = stop_time_opt {
+            let stop_date_time_naive =
+                NaiveDateTime::new(start_date_naive.to_owned(), stop_time.to_owned());
+
+            Zurich.from_local_datetime(&stop_date_time_naive).unwrap() // TODO: Better error handling stop date
+        } else {
+            start_date + Duration::hours(DEFAULT_EVENT_DURATION_HOURS)
+        };
+        dbg!(Some((
+            start_date.naive_utc().format(ICAL_STR_FORMAT).to_string(),
+            stop_date.naive_utc().format(ICAL_STR_FORMAT).to_string(),
+        )))
+    } else {
+        None
+    }
+}
+
+fn write_ics_file(
+    person_to_scene_and_schedule_entry: &Vec<(Person, Vec<&(&ScheduleEntry, &SceneEntry)>)>,
+    out_dir: &String,
+) -> std::io::Result<()> {
+    if person_to_scene_and_schedule_entry.is_empty() {
+        return Ok(());
+    }
+
+    std::fs::create_dir_all(out_dir)?;
+
+    for (person, schedule_to_scene_entries) in person_to_scene_and_schedule_entry {
+        // dtsart.format("%Y%m%dT%H%M%SZ").to_string()
+        let mut calendar = ICalendar::new("2.0", "-//EnsembLee//NONSGML Scene Scheduler//DE");
+        for (schedule_entry, scene_entry) in schedule_to_scene_entries {
+            // TODO: filter for silet play
+            if let Some((start_date_time, stop_date_time)) = get_start_and_end_time(&schedule_entry)
+            {
+                // create event which contains the information regarding the conference
+                // add properties
+                let mut event = Event::new(
+                    format!("{:x}", schedule_entry.uuid),
+                    chrono::Utc::now().format(ICAL_STR_FORMAT).to_string(),
+                );
+                event.push(DtStart::new(start_date_time));
+                event.push(DtEnd::new(stop_date_time));
+                event.push(Status::confirmed());
+                event.push(Summary::new("Theater"));
+                // Values that are "TEXT" must be escaped (only if the text contains a comma,
+                // semicolon, backslash or newline).
+                let description = format!(
+                    "Role: {}\nScenen: {}",
+                    scene_entry.role,
+                    schedule_entry.scenes.join("/")
+                );
+                event.push(Description::new(escape_text(description)));
+                // add event to calendar
+                calendar.add_event(event);
+            } else {
+                // No date specified yet
+                continue;
+            }
+        }
+
+        // write calendar to file
+        calendar.save_file(format!("{}/{}.ics", out_dir, person))?;
+    }
+    Ok(())
+}
+
+const ICAL_STR_FORMAT: &str = "%Y%m%dT%H%M%SZ";
+const DEFAULT_EVENT_DURATION_HOURS: i64 = 4;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let root_dir = Path::new(file!()).parent().and_then(|p| p.parent()).expect(
@@ -280,7 +383,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let scene_entries = parse_scene_plan_content(scene_excel_range)?;
     dbg!(&scene_entries);
     let schedule_to_scene_entries = get_schedule_to_scene_entry(&schedule_entries, &scene_entries);
-    dbg!(schedule_to_scene_entries);
+    dbg!(&schedule_to_scene_entries);
+    let person_to_schedule_and_scene_entries =
+        get_person_to_scene_and_schedule_entry(&schedule_to_scene_entries);
+
+    dbg!(&person_to_schedule_and_scene_entries);
+    let out_dir = String::from("ics");
+    write_ics_file(&person_to_schedule_and_scene_entries, &out_dir)?;
 
     Ok(())
 }
