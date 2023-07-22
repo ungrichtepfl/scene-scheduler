b7994a5e9a1af0620cc0249b4de8c9dffd1c45d0
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-07-22 19:10:12 +0200

    chore(io): Improved error message.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,13 +1,19 @@
 pub mod parsing;
 
 pub mod excel {
+  use std::io::Error;
+  use std::io::ErrorKind;
+
   use calamine::{open_workbook, DataType, Range, Reader, Xlsx};
 
   pub fn read_excel(path: &str, sheet_num: usize) -> Result<Range<DataType>, calamine::Error> {
     let mut workbook: Xlsx<_> = open_workbook(path)?;
     let range = workbook
       .worksheet_range_at(sheet_num)
-      .ok_or(calamine::Error::Msg("Cannot find sheet."))??;
+      .ok_or(calamine::Error::Io(Error::new(
+        ErrorKind::NotFound,
+        format!("Cannot find sheet number {} in file {}.", sheet_num, path),
+      )))??;
     Ok(range)
   }
 }
